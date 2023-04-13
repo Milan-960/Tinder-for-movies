4f460367e141597bb1c2d1a951cfedcbd3e758da
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-04-13 16:16:10 +0200

    text added to check the ci cd on production

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <div className="App">
         <header className="App-header">
-          <h1>Tinder for Movies!!</h1>
+          <h1>Welcome to Tinder for Movies</h1>
         </header>
       </div>
 
